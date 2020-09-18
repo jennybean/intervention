4af8a0a24bb848035a665d7d0d008a074a33003f
Ada323 <gelsey.molly@gmail.com>
2020-09-18 11:15:43 -0500

    Email styling - WIP

--- test/mailers/previews/team_lead_mailer_preview.rb
+++ test/mailers/previews/team_lead_mailer_preview.rb
@@ -1,10 +1,11 @@
 # Preview all emails at http://localhost:3000/rails/mailers/team_lead_mailer
 class TeamLeadMailerPreview < ActionMailer::Preview
   TeamLeadMailer.with(
-    team_leads: User.where(id: [1,2]),
+    project_name: 'Roci',
+    team_lead_email: "test@example.com",
     question_text: "Do you feel unproductive this week?",
     yes_votes_count: 4,
     no_votes_count: 2,
-    url: 'localhost:3000'
+    url: 'http://localhost:3000'
   ).intervention_email
 end
